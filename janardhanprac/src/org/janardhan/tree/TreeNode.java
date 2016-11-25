45613dcc91acc066d624b06dfd8ccbd77eae2d97
jd <jd@janardhanhome>
2016-11-25 10:53:16 +0530

    Added tree package with tree node class
    Change-Id: I7e2d5a60f59c3a938df174d37ee5b6c333faab45
    Signed-off-by: jd <jd@janardhanhome>

---
+++ janardhanprac/src/org/janardhan/tree/TreeNode.java
@@ -0,0 +1,17 @@
+package org.janardhan.tree;
+
+/**
+ * 
+ * @author jd. This Class containing left, right node and int data.
+ */
+
+public class TreeNode {
+
+	int data;
+	TreeNode leftNode, rightNode;
+
+	public TreeNode(int data) {
+		this.data = data;
+		this.leftNode = this.rightNode = null;
+	}
+}
