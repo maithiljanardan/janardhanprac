59a710891f51896e57a66bc5c86c19702f625887
Janardhan Maithil <maithiljanardan@gmail.com>
2017-02-18 23:59:09 +0530

    added implementation for flattening a linkedlist and commented it since new node with down and right pointers needs to be created and i am too lazy today

--- janardhanprac/src/org/janardhan/datastructure/LinkedList.java
+++ janardhanprac/src/org/janardhan/datastructure/LinkedList.java
@@ -825,6 +825,14 @@ public class LinkedList<T> implements Iterable<T> {
 		return res;
 	}
 
+	/**
+	 * Adds contents of two linked lists and return the head node of resultant
+	 * list (alternative approach)
+	 * 
+	 * @param first
+	 * @param second
+	 * @return
+	 */
 	public SinglyListNode<T> addTwoList(SinglyListNode<T> first, SinglyListNode<T> second) {
 
 		// set resultant list as null;
@@ -902,6 +910,34 @@ public class LinkedList<T> implements Iterable<T> {
 		return size;
 
 	}
+
+	/*
+	 * // An utility function to merge two sorted linked lists Node merge(Node
+	 * a, Node b) { // if first linked list is empty then second // is the
+	 * answer if (a == null) return b;
+	 * 
+	 * // if second linked list is empty then first // is the result if (b ==
+	 * null) return a;
+	 * 
+	 * // compare the data members of the two lonked lists // and put the larger
+	 * one in the result Node result;
+	 * 
+	 * if (a.data < b.data) { result = a; result.down = merge(a.down, b); }
+	 * 
+	 * else { result = b; result.down = merge(a, b.down); }
+	 * 
+	 * return result; }
+	 * 
+	 * Node flatten(Node root) { // Base Cases if (root == null || root.right ==
+	 * null) return root;
+	 * 
+	 * // recur for list on right root.right = flatten(root.right);
+	 * 
+	 * // now merge root = merge(root, root.right);
+	 * 
+	 * // return the root // it will be in turn merged with its left return
+	 * root; }
+	 */
 
 	///////////////////////////////////////////////////////////////////////////////////////////////////
 	@Override
